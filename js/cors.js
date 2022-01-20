1724c1eee9e78fb79a541a620e506b9993c56510
Vallek <vforval86@gmail.com>
2022-01-20 15:29:27 +0300

    отменил ошибочные изменения из теста

--- js/cors.js
+++ js/cors.js
@@ -6,8 +6,7 @@ button.addEventListener('click', fetchF);
 // Для использования Fetch API мы можем использовать асинхронную функцию
 async function fetchF() {
 	// Успешный запрос вернет объект Response, а неуспешный ошибку. await задает ожидание ответа от сервера
-	const response = await fetch('https://github.com/Vallek/');
-	console.error(response);
+	const response = await fetch('https://api.github.com/users/Vallek/followers');
 	// Если ответ успешен, то 
 	if (response.ok) {
 		// берем json данные из объекта
