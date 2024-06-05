1085f4efba320ae613061a633581d5a38ef7debd
Vallek <vforval86@gmail.com>
2024-06-05 18:29:55 +0300

    Добавил навигацию для мыши, рефакторинг

---
+++ js/nav.js
@@ -0,0 +1,31 @@
+const navbar = document.querySelector('.nav');
+const navBack = navbar.querySelector('.nav__btn_back');
+const navForward = navbar.querySelector('.nav__btn_forward');
+const navAll = navbar.querySelector('.nav__btn_all');
+
+const backEvent = new KeyboardEvent("keydown", {
+  key: "ArrowLeft",
+  keyCode: 37,
+  which: 37
+});
+navBack.addEventListener('click', (el) => {
+	document.dispatchEvent(backEvent);
+});
+
+const forwardEvent = new KeyboardEvent("keydown", {
+  key: "ArrowRight",
+  keyCode: 39,
+  which: 39
+});
+navForward.addEventListener('click', (el) => {
+	document.dispatchEvent(forwardEvent);
+});
+
+const allEvent = new KeyboardEvent("keydown", {
+  key: "Esc",
+  keyCode: 27,
+  which: 27
+});
+navAll.addEventListener('click', (el) => {
+	document.dispatchEvent(allEvent);
+});
